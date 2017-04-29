78fc941086e581526fb0fad8cbae23e776a5e8da
Ethan Chao <ethanlikescpp@users.noreply.github.com>
2017-04-29 13:26:33 -0700

    Pretty much done
    when button is triggered, corresponding pin set to HIGH

--- LaunchPadSide.ino
+++ LaunchPadSide.ino
@@ -8,19 +8,21 @@
    Ethan Chao 2017  
 ------------------------------------------------------------------------------
 List of things this program must do ($Tested, #Implemented, %To do):
-%Receive the register and parse using StringObject: https://www.arduino.cc/en/Reference/StringObject
-%Confirm message fidelity with the checksum
+#Receive the register and parse using StringObject: https://www.arduino.cc/en/Reference/StringObject
+#Confirm message fidelity with the checksum
 %Read register state and apply it to the pins (LOW When triggered)
 
                                                                              
  */
-//uint16_t statedata = 0b1000000000000000; //The 16 bit register I'm using to store the button states to be received
+uint16_t statedata = 0b1000000000000000; //The 16 bit register I'm using to store the button states to be received
 FastCRC8 CRC8; //Some kind of setup for CRC8 usage
 void receivecommand();
 void parsetransmission(String);
 uint16_t parsedata(String);
 int parsechecksum(String);
 bool datacheck(uint16_t, int);
+int xorchecksum(uint16_t);
+void applycommand();
 
 int lastTime = 0, currentTime = 0, delayTime = 1000; //Delays the loop() for 500 milliseconds
 
@@ -35,43 +37,55 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  receivecommand(); // Gets the data
+  applycommand(); // Applys the new button states
   
-  currentTime = millis(); //This whole block here is a delay
-  if(currentTime - lastTime >= delayTime) {
-    receivecommand();
-    lastTime = currentTime;
-  }
+  delay(1000);
 }
+
+
+void applycommand() {
+  for (int i = 2; i < 14; i++) {
+      if ((statedata & (10^i)) == 1) {
+        digitalWrite(i, HIGH); // When button is triggered, pin set to high
+      }
+  } 
+}
+
+
 
 void receivecommand(){ //This function receives the register
     
-  //String incomingCom = Serial.read(); // The actual receiving of the data!
-  String incomingCom = "<CP22GroundCom>32768234"; // A test string to play with
+  String incomingCom = (String) Serial.read(); // The actual receiving of the data!
+  //String incomingCom = "<CP22GroundCom>32768-128"; // A test string to play with
   if (incomingCom.startsWith("<CP22GroundCom>", 0)) { // This if statement confirms the CPSS ground control header
     //Serial.println("Yup!");
     incomingCom.remove(0, 15); // Remove packet header for additional processing
     parsetransmission(incomingCom);
     }
-    else { // If didn't receive tag, try again
-      //Serial.println("Nope!");
-      //delay(1000);
-      receivecommand();
-      }
+  else { // If didn't receive tag, try again
+    receivecommand();
+    }
        
 }
 
 // String -> Void
 // Takes in the data+checksum, then runs parse and checksum functions
 void parsetransmission(String incomingCom){
- 
-  int receivechecksum;
   
-
   uint16_t receivedata = parsedata(incomingCom); // Extract that data!
   //Serial.println(receivedata, BIN);
   int receivecheck = parsechecksum(incomingCom); // Extract that checksum!
   bool checkcheck = datacheck(receivedata, receivecheck);
-  Serial.println(checkcheck);
+  //Serial.println(checkcheck);
+  if (checkcheck == 1) {
+    //Serial.println("yay");
+    statedata = receivedata;
+  }
+  
+  else {
+    receivecommand();
+  }
 
 }
 // String -> int
@@ -94,10 +108,9 @@ uint16_t parsedata(String incomingCom) {
 // String -> int
 // Removes everything before the last two index positions in the string object and returns it as an integer
 int parsechecksum(String incomingCom) {
-  // In the example, 234 is the checksum
+  // In the example, 128 is the checksum
   String rawstuff = incomingCom;
-  int datalength = incomingCom.length();
-  datalength -= 3;
+  int datalength = rawstuff.indexOf("-") + 1;
   rawstuff.remove(0, datalength); // Remove everything before the last two index positions
   int sumdata = rawstuff.toInt();
   //Serial.println(sumdata);
@@ -107,15 +120,30 @@ int parsechecksum(String incomingCom) {
 // Int, Int -> bool
 // Verifies the data with the checksum
 bool datacheck(uint16_t indata, int inchecksum) {
-  //int checksum = CRC8.smbus(indata, 16);
   //Serial.println(checksum);
   //Serial.println(inchecksum);
-  if (CRC8.smbus(indata, 16) == inchecksum) {
+  if (xorchecksum(indata) == inchecksum) {
     //Serial.println("True");
     return true;
   }
   else {
-    //Serial.println("False");
-    return false;
+    Serial.println("False");
+    //return false;
   }
 }
+
+int xorchecksum(uint16_t indata) {
+  int checksum;
+  union {
+    uint16_t longdata;
+    uint8_t shortdata [2];
+  } data;
+  data.longdata = indata;
+  for (int i = 0; i < 2; i++) {
+    checksum = checksum ^ data.shortdata[i];
+  }
+  //Serial.println(checksum);
+  return checksum;
+  
+}
+
