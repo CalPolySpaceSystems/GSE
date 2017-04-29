b586da5dbdd81ab231a4111dae6a0f68b25aa96d
Brett Glidden <bglidden@calpoly.edu>
2017-04-29 14:23:29 -0700

    Added in folders for building in the arduino IDE

---
+++ GroundStation/GroundStation.ino
@@ -0,0 +1,91 @@
+#include "FastCRC.h"
+
+/*   _____ _____   _____ _____    _____                           _    _____ _        _   _             
+  / ____|  __ \ / ____/ ____|  / ____|                         | |  / ____| |      | | (_)            
+ | |    | |__) | (___| (___   | |  __ _ __ ___  _   _ _ __   __| | | (___ | |_ __ _| |_ _  ___  _ __  
+ | |    |  ___/ \___ \\___ \  | | |_ | '__/ _ \| | | | '_ \ / _` |  \___ \| __/ _` | __| |/ _ \| '_ \ 
+ | |____| |     ____) |___) | | |__| | | | (_) | |_| | | | | (_| |  ____) | || (_| | |_| | (_) | | | |
+  \_____|_|    |_____/_____/   \_____|_|  \___/ \__,_|_| |_|\__,_| |_____/ \__\__,_|\__|_|\___/|_| |_|
+  Ethan Chao 2017
+------------------------------------------------------------------------------------------------------
+Ground control to Major Tom...
+List of things this program must do ($Tested, #Implemented, %To do):
+
+#Store all button and switch states in a 16 bit register for Arduino pins 2 to 13 
+#Calculate a CRC8 checksum of the two bytes (16 bit register) of data using the FastCRC library
+#Configure the format of the packet to be sent
+%Setup the command confirmation on this end, received from the pad
+  
+*/
+                                                                                                                                                                                                       
+uint16_t a = 0b1000000000000000; //The 16 bit register I'm using to store the button states to transmit
+uint16_t xorchecksum(uint16_t);
+int lastTime = 0, currentTime = 0, delayTime = 500; //Delays the loop() for 500 milliseconds 
+void buttoncheck(void);
+void sendcommand();
+
+void setup() {
+  //Put your setup code here, to run once:
+  Serial.begin(9600);
+    for (int i = 2; i < 14; i++) { //Setup I/O pins 2 through 13, the max number of I/Os we can get
+      pinMode(i, INPUT_PULLUP); //Arduino provides pullups for the buttons. No need to include them in circuit
+      //Serial.println(i); //Just checking to make sure the pins are set up!
+    }
+
+}
+
+void loop() {
+  //Put your main code here, to run repeatedly:
+  buttoncheck(); //Run the buttoncheck function to get all button states and store in 16 bit register
+  //int checksum = CRC8.smbus(a, 16); //Calculation of the checksum, courtesy of the FastCRC library, stored in the "checksum" integer
+  //int checksum = CRC16.ccitt(a, 16); //CRC16 test
+  uint16_t checksum = xorchecksum(a);
+  sendcommand();
+  delay(1000);
+/*
+  currentTime = millis(); //This whole block here is a delay
+  if(currentTime - lastTime >= delayTime) {
+    
+    sendcommand();
+    lastTime = currentTime;
+  }
+*/
+}
+
+void buttoncheck() { //A function that uses a loop to check the state of each button, and stores the state in that 16 bit register I'm using
+  for (int i = 2; i < 14; i++) {
+    int state = digitalRead(i); //Takes in the state of whatever pin is being covered in the loop
+
+    if (state == LOW) { //Switches are pulled to low when triggered
+      a | (10^i); //OR operator to set the bit to one
+    }
+    else {
+      a &=~ (1 << 2); //Really complicated thing to turn the bit to zero
+    }
+  }
+}
+
+void sendcommand(){
+  uint16_t checksum = xorchecksum(a);
+  Serial.print("<CP22GroundCom>");
+  Serial.print(a); // Sends the register
+  Serial.print("-");
+  Serial.print(checksum); // Sends the checksum
+  Serial.print("\n");
+  //Serial.print("GroundCom Transmission Successful!"); //Just for testing!
+}
+
+uint16_t xorchecksum(uint16_t indata) {
+  uint16_t checksum;
+  union {
+    uint16_t longdata;
+    uint8_t shortdata [2];
+  } data;
+  data.longdata = indata;
+  for (int i = 0; i < 2; i++) {
+    checksum = checksum ^ data.shortdata[i];
+  }
+  //Serial.println(checksum);
+  return checksum;
+  
+}
---
+++ LaunchPadSide/LaunchPadSide.ino
@@ -0,0 +1,151 @@
+#include "FastCRC.h"
+/*
+   __________  __________ __                           __    ____            __
+  / ____/ __ \/ ___/ ___// /   ____ ___  ______  _____/ /_  / __ \____ _____/ /
+ / /   / /_/ /\__ \\__ \/ /   / __ `/ / / / __ \/ ___/ __ \/ /_/ / __ `/ __  / 
+/ /___/ ____/___/ /__/ / /___/ /_/ / /_/ / / / / /__/ / / / ____/ /_/ / /_/ /  
+\____/_/    /____/____/_____/\__,_/\__,_/_/ /_/\___/_/ /_/_/    \__,_/\__,_/   
+   Ethan Chao 2017  
+------------------------------------------------------------------------------
+List of things this program must do ($Tested, #Implemented, %To do):
+#Receive the register and parse using StringObject: https://www.arduino.cc/en/Reference/StringObject
+#Confirm message fidelity with the checksum
+%Read register state and apply it to the pins (LOW When triggered)
+
+                                                                             
+ */
+uint16_t statedata = 0b1000000000000000; //The 16 bit register I'm using to store the button states to be received
+FastCRC8 CRC8; //Some kind of setup for CRC8 usage
+void receivecommand();
+void parsetransmission(String);
+uint16_t parsedata(String);
+uint16_t parsechecksum(String);
+bool datacheck(uint16_t, int);
+uint16_t xorchecksum(uint16_t);
+void applycommand();
+
+int lastTime = 0, currentTime = 0, delayTime = 1000; //Delays the loop() for 500 milliseconds
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for (int i = 2; i < 14; i++) { //Setup I/O pins 2 through 13, the max number of I/Os we can get
+    pinMode(i, OUTPUT); //Configure all avaliable I/Os for output 
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial.available()) {
+    receivecommand(); // Gets the data
+    applycommand(); // Applys the new button states
+  }
+  /* FOR DEBUGGING */
+  Serial.println(statedata);
+}
+
+
+void applycommand() {
+  for (uint16_t i = 2; i < 14; i++) {
+      if ((statedata & (10^i)) == 1) {
+        digitalWrite(i, HIGH); // When button is triggered, pin set to high
+      }
+  } 
+}
+
+
+
+void receivecommand(){ //This function receives the register
+    
+  String incomingCom = (String) Serial.read(); // The actual receiving of the data!
+  //String incomingCom = "<CP22GroundCom>32768-128"; // A test string to play with
+  if (incomingCom.startsWith("<CP22GroundCom>", 0)) { // This if statement confirms the CPSS ground control header
+    //Serial.println("Yup!");
+    incomingCom.remove(0, 15); // Remove packet header for additional processing
+    parsetransmission(incomingCom);
+    }
+  else { // If didn't receive tag, try again
+    receivecommand();
+    }
+       
+}
+
+// String -> Void
+// Takes in the data+checksum, then runs parse and checksum functions
+void parsetransmission(String incomingCom){
+  
+  uint16_t receivedata = parsedata(incomingCom); // Extract that data!
+  //Serial.println(receivedata, BIN);
+  uint16_t receivecheck = parsechecksum(incomingCom); // Extract that checksum!
+  bool checkcheck = datacheck(receivedata, receivecheck);
+  //Serial.println(checkcheck);
+  if (checkcheck == 1) {
+    //Serial.println("yay");
+    statedata = receivedata;
+  }
+  
+  else {
+    receivecommand();
+  }
+
+}
+// String -> int
+// Removes the last two index positions of the String where the checksum is, and return the data as a 16 bit unsigned integer
+uint16_t parsedata(String incomingCom) {
+  // In the example, 32768 is the data
+  String rawdata = incomingCom;
+  int datalength = rawdata.length();
+  datalength -= 3;
+  rawdata.remove(datalength); // Delete the last two digits
+  int intdata = rawdata.toInt(); // Convert the data to int
+  //Serial.println(rawdata);
+  uint16_t inunsighned;
+  inunsighned = (uint16_t) intdata; // Cast int to unsigned 16 bit integer
+  //Serial.println(inunsighned, BIN);
+  return inunsighned;
+  
+}
+
+// String -> int
+// Removes everything before the last two index positions in the string object and returns it as an integer
+uint16_t parsechecksum(String incomingCom) {
+  // In the example, 128 is the checksum
+  String rawstuff = incomingCom;
+  int datalength = rawstuff.indexOf("-") + 1;
+  rawstuff.remove(0, datalength); // Remove everything before the last two index positions
+  uint16_t sumdata = rawstuff.toInt();
+  //Serial.println(sumdata);
+  return sumdata;
+}
+
+// Int, Int -> bool
+// Verifies the data with the checksum
+bool datacheck(uint16_t indata, int inchecksum) {
+  //Serial.println(checksum);
+  //Serial.println(inchecksum);
+  if (xorchecksum(indata) == inchecksum) {
+    //Serial.println("True");
+    return true;
+  }
+  else {
+    Serial.println("False");
+    //return false;
+  }
+}
+
+uint16_t xorchecksum(uint16_t indata) {
+  uint16_t checksum;
+  union {
+    uint16_t longdata;
+    uint8_t shortdata [2];
+  } data;
+  data.longdata = indata;
+  for (int i = 0; i < 2; i++) {
+    checksum = checksum ^ data.shortdata[i];
+  }
+  //Serial.println(checksum);
+  return checksum;
+  
+}
+
